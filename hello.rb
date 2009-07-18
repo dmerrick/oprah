c08d1785f3ab78423806d3dbf332a57cd18f23e6
Dana Merrick <dana@ics.com>
2009-07-18 13:09:04 -0500

    Beefing up the application.

--- hello.rb
+++ hello.rb
@@ -1,20 +1,35 @@
 require 'rubygems'
   require 'sinatra'
   require 'dm-core'
+  require 'dm-timestamps'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
 
-get '/:url' do |url|
-  content_type "text/plain"
-
-  request.env['REMOTE_ADDR'].split(",").first
-  url
-
-  #Hit.new(:url => url).save
-end
-
 get '/hits' do
   Hit.all.each do |hit|
-    hit.inspect
+    hit.ip
+    hit.url
   end
 end
+
+get '/:url' do
+  content_type "text/plain"
+
+  ip = request.env['REMOTE_ADDR'].split(",").first
+  Hit.new(:ip => ip, :url => params[:url]).save
+
+  ip
+end
+
+class Hit
+  include DataMapper::Resource
+
+  property :id, Serial
+
+  propery :url, String
+  propery :ip, String
+
+  property :created_at, DateTime
+
+end
+
