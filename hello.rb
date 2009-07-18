351cd9f364ce404ce22b815955db1ee256d97e32
Dana Merrick <dana@ics.com>
2009-07-18 12:41:12 -0500

    Now shows your IP.

--- hello.rb
+++ hello.rb
@@ -1,5 +1,19 @@
-require 'sinatra'
+require 'rubygems'
+  require 'sinatra'
 
-get '/' do
-  "Hello World!"
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
+
+get '/:url' do |url|
+  content_type "text/plain"
+
+  request.env['REMOTE_ADDR'].split(",").first
+  url
+
+  #Hit.new(:url => url).save
+end
+
+get '/hits' do
+  Hit.all.each do |hit|
+    hit.inspect
+  end
 end
