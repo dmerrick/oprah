1fbcddf645d64d68033fd6e8cf34fd7a41223582
Dana Merrick <dana@ics.com>
2009-07-18 19:11:09 -0500

    More cleanup and features.

--- hello.rb
+++ hello.rb
@@ -1,29 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+
 require 'rubygems'
   require 'sinatra'
   require 'dm-core'
   require 'dm-timestamps'
 
-require 'csv'
-
+# set up Sinatra
 configure do
   DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3:development.db')
   DataMapper.auto_upgrade!
-end
 
-set :root, File.dirname(__FILE__)
-disable :static
+  set :root, File.dirname(__FILE__)
+  disable :static
+end
 
 # show the environment information
 get '/env' do
   content_type 'text/plain'
   require 'pp'
-  ENV.pretty_inspect
+  # DATABASE_URL contains database login info
+  ENV.reject {|k,v| k=="DATABASE_URL"}.pretty_inspect
 end
 
+# alias for the hits file
 get '/hits' do
   redirect '/hits.csv'
 end
 
+# auto-generate hits file
 get '/hits.csv' do
   content_type "text/csv"
 
@@ -43,16 +49,17 @@ get '/hits.csv' do
   report.readlines
 end
 
+# keep browsers from looking for favicon
 get 'favicon.ico' do
   halt 403
 end
 
+# show IP and log everything else
 get '/?*' do
   content_type "text/plain"
 
   ip = request.env['REMOTE_ADDR'].split(",").first
-  url = params[:splat].first
-  url = "(blank)" if url.blank?
+  url = params[:splat].first || "(blank)"
 
   Hit.new(:ip => ip, :url => url).save
 
@@ -71,4 +78,4 @@ class Hit
 
 end
 
-Hit.auto_migrate!
+Hit.auto_upgrade!
