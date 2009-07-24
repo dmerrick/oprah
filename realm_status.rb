9cd7852edcc2db0aee3d25e6bd887eac105dbbea
Dana Merrick <dmerrick@dmerrick-dt-2.local>
2009-07-24 13:29:11 -0500

    Gracefully handle errors.

--- realm_status.rb
+++ realm_status.rb
@@ -8,10 +8,13 @@ get '/realm/:realm' do
   realm = params[:realm]
 
   xml_data = XmlSimple.xml_in(open(status_page))
-  status = xml_data["rs"].first["r"].select {|r| r["n"] == realm.capitalize }.first["s"]
+  status = xml_data["rs"].first["r"].select {|r| r["n"] == realm.capitalize }.first["s"] rescue "error"
 
   if status == "1"
     "up"
+  elsif status == "error"
+    record_hit
+    "error"
   else
     "down"
   end
